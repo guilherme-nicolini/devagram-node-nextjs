8e194afe389e62de1aa994fadc255d390afda730
Guilherme Nicolini <guilhermenicolini1@gmail.com>
2023-03-30 15:30:17 -0300

    Criado Usuario e criptografia da senha

--- pages/api/cadastro.ts
+++ pages/api/cadastro.ts
@@ -1,28 +1,43 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import type { RespostaPadraoMsg } from "../../types/RespostaPadraoMsg";
 import type { CadastroRequisicao } from "../../types/CadastroRequisicao";
+import { UsuarioModel } from "../../models/UsuarioModel";
+import { conectarMongoDb } from "../../middlewares/conectarMongoDb";
+import md5 from 'md5';
 
-const endpointCadastro = (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg>) => {
-    if (req.method === 'POST') {
-        const usuario = req.body as CadastroRequisicao;
+const endpointCadastro =
+    async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg>) => {
+        if (req.method === 'POST') {
+            const usuario = req.body as CadastroRequisicao;
 
-        if (!usuario.nome || usuario.nome.length < 2) {
-            return res.status(400).json({ erro: 'Nome invalido' });
+            if (!usuario.nome || usuario.nome.length < 2) {
+                return res.status(400).json({ erro: 'Nome invalido' });
+            }
+
+            if (!usuario.email || usuario.email.length < 5
+                || !usuario.email.includes('@')
+                || !usuario.email.includes('.')) {
+                return res.status(400).json({ erro: 'Email invalido' });
+            }
+
+            if (!usuario.senha || usuario.senha.length < 4) {
+                return res.status(400).json({ erro: 'Senha invalida' });
+            }
+            const usuarioComMesmoEmail = await UsuarioModel.find({ email: usuario.email });
+            if (usuarioComMesmoEmail && usuarioComMesmoEmail.length > 0) {
+                return res.status(400).json({ erro: `ja existe uma conta com o email informado` });
+            }
+
+
+            const usuarioASerSalvo = {
+                nome: usuario.nome,
+                email: usuario.email,
+                senha: md5(usuario.senha)
+            }
+            await UsuarioModel.create(usuarioASerSalvo);
+            return res.status(200).json({ msg: 'Usuário criado com sucesso' });
         }
 
-        if (!usuario.email || usuario.email.length < 5
-            || !usuario.email.includes('@')
-            || !usuario.email.includes('.')) {
-            return res.status(400).json({ erro: 'Email invalido' });
-        }
-        
-        if (!usuario.senha || usuario.senha.length < 4) {
-            return res.status(400).json({ erro: 'Senha invalida' });
-        }
-
-        return res.status(200).json({msg:'Dados Corretos'});
+        return res.status(405).json({ erro: 'Método informado não é válido' });
     }
-
-    return res.status(405).json({ erro: 'Método informado não é válido' });
-}
-export default endpointCadastro;
+export default conectarMongoDb(endpointCadastro);
--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,18 +1,24 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { conectarMongoDb } from '../../middlewares/conectarMongoDb';
 import { RespostaPadraoMsg } from '../../types/RespostaPadraoMsg';
+import md5 from 'md5';
+import { UsuarioModel } from '../../models/UsuarioModel';
 
-const endpointLogin = (
+const endpointLogin = async (
     req: NextApiRequest,
     res: NextApiResponse<RespostaPadraoMsg>
 ) => {
     if (req.method === 'POST') {
         const { login, senha } = req.body;
 
-        if (login === 'admin@admin.com' &&
-            senha === "admin@123") {
-            return res.status(200).json({ msg: "Usuário autenticado com sucesso" })
+        const usuarioEncontrados = await UsuarioModel.find({ email: login, senha: md5(senha) });
+
+        if (usuarioEncontrados && usuarioEncontrados.length > 0) {
+            const usuarioEncontrado = usuarioEncontrados[0];
+            return res.status(200).json({ msg: `Usuario ${usuarioEncontrado.nome}autenticado com sucesso`})
+
         }
+       
         return res.status(400).json({ erro: "Usuário não encontrado" })
     }
     return res.status(405).json({ erro: 'Método informado não é válido' });
